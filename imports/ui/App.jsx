178852f7acf25641feb19dd680d27d5bf1b3de05
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-05-05 21:17:06 -0300

    Criado menu lateral

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -12,12 +12,69 @@ export const App = () => {
     <div className="main">
       {user ? (
         <Fragment>
-          <div className="user" onClick={logout}>
-            {user.username} 🚪
-          </div>
+          <nav id="sidebar">
+            <div className="sidebar-header">
+              <h3>Proffy</h3>
+            </div>
 
-          <p>BEM VINDO</p>
-          <Routes />
+            <ul>
+              <p>Dummy Heading</p>
+              <li className="active">
+                <a
+                  href="#homeSubmenu"
+                  data-toggle="collapse"
+                  aria-expanded="false"
+                  className="dropdown-toggle"
+                >
+                  Home
+                </a>
+                <ul className="collapse list-unstyled" id="homeSubmenu">
+                  <li>
+                    <a href="#">Home 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Home 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Home 3</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a
+                  href="#pageSubmenu"
+                  data-toggle="collapse"
+                  aria-expanded="false"
+                  className="dropdown-toggle"
+                >
+                  Pages
+                </a>
+                <ul className="collapse list-unstyled" id="pageSubmenu">
+                  <li>
+                    <a href="#">Page 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Page 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Page 3</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <div id="content">
+            <Routes />
+          </div>
         </Fragment>
       ) : (
         <Fragment>
